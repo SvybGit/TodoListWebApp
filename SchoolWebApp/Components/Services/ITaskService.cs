3e83ff15794f2ec335f1579b504569b4fcd98c14
Šimon Vybíral <simon.vybiral@outlook.com>
2025-02-12 16:08:32 +0100

    Přidejte soubory projektu.

---
+++ SchoolWebApp/Components/Services/ITaskService.cs
@@ -0,0 +1,12 @@
+﻿using static SchoolWebApp.Components.Pages.HomePage;
+
+namespace SchoolWebApp.Components.Services
+{
+    public interface ITaskService
+    {
+        public void Add(taskItem task);
+        public void Delete(taskItem task);
+        public void Edit(taskItem task);
+        public List<taskItem> GetTaskList();
+    }
+}
---
+++ SchoolWebApp/Components/Services/TaskService.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using static SchoolWebApp.Components.Pages.HomePage;
+
+namespace SchoolWebApp.Components.Services
+{
+    public class TaskService : ITaskService
+    {
+        private List<taskItem> taskList;
+        public TaskService()
+        {
+            taskList = new List<taskItem>();
+            taskList.Add(new taskItem("Task 1", "This is the first task"));
+            taskList.Add(new taskItem("Task 2", "This is the second task"));
+            taskList.Add(new taskItem("Task 3", "Lorem ipsum aaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa "));
+            taskList.Add(new taskItem("Task 3", "Lorem ipsum aaaaaaaaa"));
+        }
+        public void Add(taskItem task)
+        {
+            taskList.Add(task);
+        }
+        public void Delete(taskItem task)
+        {
+            Console.WriteLine("Task Deleted in service");
+            taskList.Remove(task);
+        }
+        public void Edit(taskItem task)
+        {
+            taskList[taskList.IndexOf(task)] = task;
+        }
+
+        public List<taskItem> GetTaskList()
+        {
+            return taskList;
+        }
+    }
+}
